53e319cb0f723ca233740779e83705de2e2d1532
abhay2002-pro <abhayray2002@gmail.com>
2023-01-07 20:24:07 +0530

    added check for not logged in user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,13 @@
 import { catchAsyncError } from "./catchAsyncError.js";
+import ErrorHandler from "../utils/errorHandler.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  console.log("token ", token);
 
-  if (token == null) return res.sendStatus(404);
+  if (token == null) return next(new ErrorHandler("Not Logged In", 401));
+
   jwt.verify(token, process.env.JWT_SECRET, (err, id) => {
     if (err) return res.sendStatus(403);
     req.user = id;
