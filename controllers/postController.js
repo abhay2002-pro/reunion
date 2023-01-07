9719b529b0453e26553e94c4604ae7b3f1729026
abhay2002-pro <abhayray2002@gmail.com>
2023-01-07 22:23:32 +0530

    dislike post API added

--- controllers/postController.js
+++ controllers/postController.js
@@ -45,12 +45,33 @@ export const likePost = catchAsyncError(async (req, res, next) => {
 
   const post_id = req.params.id;
 
-  const post = await Post.findOne({_id: post_id});
+  const post = await Post.findOne({ _id: post_id });
   if (!post) return next(new ErrorHandler("Post not found", 404));
 
   post.likes.push(user);
   await post.save();
-  
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+export const dislikePost = catchAsyncError(async (req, res, next) => {
+  const _id = req.user._id;
+  const user = await User.findOne({ _id });
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  const post_id = req.params.id;
+
+  const post = await Post.findOne({ _id: post_id });
+  if (!post) return next(new ErrorHandler("Post not found", 404));
+
+  post.likes = post.likes.filter((curr_user) => {
+    return curr_user !== user._id;
+  });
+
+  await post.save();
+
   res.status(200).json({
     success: true,
   });
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { addPost, deletePost, likePost } from "../controllers/postController.js";
+import { addPost, deletePost, likePost, dislikePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.route("/posts").post(isAuthenticated, addPost);
 router.route("/posts/:id").delete(isAuthenticated, deletePost);
 router.route("/like/:id").post(isAuthenticated, likePost);
+router.route("/dislike/:id").post(isAuthenticated, dislikePost);
 
 export default router;
