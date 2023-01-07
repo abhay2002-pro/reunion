e942e38a01dc70baa1d31e1ce49aa37dcf90d602
abhay2002-pro <abhayray2002@gmail.com>
2023-01-07 12:57:07 +0530

    created comment model

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  description: {
+    type: String,
+    max: 500,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = mongoose.model("User", schema);
