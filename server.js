1480050f76f4ce3f12d2c468c08ef10e4f1507e1
abhay2002-pro <abhayray2002@gmail.com>
2023-01-09 14:37:02 +0530

    fixing typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import { connectDB } from "./config/database.js";
 
 connectDB();
-PORT = procees.env.PORT || 4000;
+PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is working on port: ${PORT}`);
 });
