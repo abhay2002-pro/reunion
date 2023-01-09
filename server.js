64ddd0c9ab7d25de68ed8b09a04fdcb7d99f65ca
abhay2002-pro <abhayray2002@gmail.com>
2023-01-09 14:28:49 +0530

    changed port in server

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ import app from "./app.js";
 import { connectDB } from "./config/database.js";
 
 connectDB();
-
-const PORT = 4000;
+PORT = procees.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is working on port: ${PORT}`);
 });
