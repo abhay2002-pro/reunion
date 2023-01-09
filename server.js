2dadde9a1f8ec80d52e1b549fcc86fccce58f544
abhay2002-pro <abhayray2002@gmail.com>
2023-01-09 14:42:51 +0530

    fixing error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import { connectDB } from "./config/database.js";
 
 connectDB();
-PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is working on port: ${PORT}`);
 });
